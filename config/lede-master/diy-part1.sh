e1bd7e53e9365cfa3ba4c1d950e21f8407f72a3b
lj8812 <74142076+lj8812@users.noreply.github.com>
2023-09-28 12:08:55 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -12,3 +12,70 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+##############################################################################################
+
+# 主题
+git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-argonne
+
+##############################################################################################
+
+
+# 状态
+
+# 实时监控 lede包已有 最近有bug
+
+# 释放内存 lede包已有
+# git clone https://github.com/chinaboy2345/luci-app-ramfree.git package/luci-app-ramfree
+
+##############################################################################################
+
+#  系统
+
+#定时重启
+#git clone https://github.com/f8q8/luci-app-autoreboot.git package/luci-app-autoreboot
+
+#ttyd lede包已有
+
+#磁盘管理
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
+#文件传输 lede包已有
+
+#  关机
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+
+##############################################################################################
+
+# 服务
+
+#ShadowSocksR Plus+
+git clone https://github.com/fw876/helloworld.git package/helloworld
+
+#passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+
+#helloword
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/fw876/helloworld.git package/helloworld
+
+#openclash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
+#luci-app-adguardhome
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+
+#luci-app-adbyby-plus
+git clone https://github.com/ywt114/luci-app-adbyby-plus package/luci-app-adbyby-plus
+
+##############################################################################################
+
+# 网络
+
+#Turbo ACC 加速 lede包已有
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/luci-app-turboacc
+
+
+##############################################################################################
+
